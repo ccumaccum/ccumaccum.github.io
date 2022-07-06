f1b1cee98f2661d35ebbec0047dd871e04f746b2
ccumaccum <108814803+ccumaccum@users.noreply.github.com>
2022-07-06 23:54:53 +0900

    스케일

--- script/script.js
+++ script/script.js
@@ -4,7 +4,8 @@ window.onload = ()=>{
     var mql = window.matchMedia("screen and (max-width: 720px)");
     var width = window.outerWidth;
     var scale = width/720;
+    var wrap = document.getElementById('wrap');
     if (mql.matches) {
-        document.getElementById('wrap').style.transform = `scale(${scale})`;
+        wrap.style.transform = `scale(${scale})`;
     }
 }
