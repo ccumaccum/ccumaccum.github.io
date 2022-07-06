a3d18151b3bc7d117d366c93999ce2aa2e85af24
ccumaccum <108814803+ccumaccum@users.noreply.github.com>
2022-07-06 23:48:04 +0900

    스케일

--- script/script.js
+++ script/script.js
@@ -1,9 +1,10 @@
-var mql = window.matchMedia("screen and (max-width: 720px)");
-var width = window.outerWidth;
-var scale = width/720;
+
 
 window.onload = ()=>{
+    var mql = window.matchMedia("screen and (max-width: 720px)");
+    var width = window.outerWidth;
+    var scale = width/720;
     if (mql.matches) {
-        document.body.style.transform = `scale(${scale})`;
+        document.getElementById('wrap').style.transform = `scale(${scale})`;
     }
 }
